71884eb1c1e1a0b2ffabcd6e389c6588348cdb56
RaisulislamRimon <rimonppip0@gmail.com>
2022-10-27 02:48:58 +0600

    added sweet alert for clicking pay now button in checkout page

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Checkout = () => {
   const course = useLoaderData();
   const { name, price, image } = course;
+  const handlePayNow = () => {
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: "You have successfully paid for this course",
+      showConfirmButton: false,
+      timer: 5000,
+    });
+  };
   return (
     <div>
-      <h1 className="text-4xl text-center mt-5 text-primary underline">Checkout</h1>
+      <h1 className="text-4xl text-center mt-5 text-primary underline">
+        Checkout
+      </h1>
       <div className="card lg:card-side bg-base-100 shadow-xl p-28">
         <figure>
           <img className="w-1/3" src={image} alt={name} />
@@ -18,8 +30,15 @@ const Checkout = () => {
           <p className="text-stone-800 text-base">
             <span className="text-2xl">Price : ${price} </span>
           </p>
+
+          <p className="text-2xl">Click to pay: </p>
+
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Listen</button>
+            <button className="btn btn-primary">
+              <span onClick={handlePayNow} className="text-2xl">
+                Pay Now
+              </span>
+            </button>
           </div>
         </div>
       </div>
