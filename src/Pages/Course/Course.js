645d49ee7bae0853864c68ac5b91a9862ed002a0
RaisulislamRimon <rimonppip0@gmail.com>
2022-10-27 01:27:45 +0600

    updated course style and content.

--- src/Pages/Course/Course.js
+++ src/Pages/Course/Course.js
@@ -5,13 +5,17 @@ const Course = ({ course }) => {
   const { id, name, price, description, image } = course;
   return (
     <div className="">
-      <div className="card card-compact w-full bg-base-100 shadow-xl hover:border">
+      <div className="card card-compact w-4/5 my-5 mx-10 bg-base-100 shadow-2xl hover:drop-shadow-2xl">
         <figure>
           <img src={image} alt={name} className="w-48 my-10" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">{name}</h2>
-          <p>{description}</p>
+          <h2 className="card-title mx-auto text-3xl">{name}</h2>
+          <p className="text-stone-800 text-base">{description}</p>
+          <p className="text-stone-800 text-base font-semibold">
+            Price : ${price}
+          </p>
+
           <div className="card-actions justify-end">
             <button className="btn btn-primary">
               <Link to={`/course/${id}`}>View Course</Link>
