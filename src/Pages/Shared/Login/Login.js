2257a6140c921ae85f06cad0e2e6e425f3e9288a
RaisulislamRimon <rimonppip0@gmail.com>
2022-10-26 14:33:05 +0600

    setup login page.

--- src/Pages/Shared/Login/Login.js
+++ src/Pages/Shared/Login/Login.js
@@ -1,34 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [error, setError] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    if (password.length < 6) {
+      setError("Password must be at least 6 characters");
+      return;
+    }
+  };
   return (
     <div>
-      <h1 className="text-3xl">Login</h1>
-      <form className="form-control w-full max-w-xs">
-        <label className="label">
-          <span className="label-text">Enter your email</span>
-          <span className="label-text-alt text-warning text-lg p-0 m-0">*</span>
-        </label>
-        <input
-          type="email"
-          placeholder="email"
-          className="input input-bordered w-full max-w-xs"
-          required
-        />
-        <label className="label">
-          <span className="label-text">Enter your password</span>
-          <span className="label-text-alt text-warning text-lg p-0 m-0">*</span>
-        </label>
-        <input
-          type="password"
-          placeholder="password"
-          className="input input-bordered w-full max-w-xs"
-          required
-        />
-        <button className="btn btn-wide btn-active text-white btn-accent">
-          Login
-        </button>
-      </form>
+      <div>
+        <h2 className="text-center text-4xl font-bold mb-10">Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-control w-full max-w-xs mx-auto mb-10">
+            <div className="mb-5">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="input input-bordered w-full max-w-xs"
+              />
+            </div>
+            <div>
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                className="input input-bordered w-full max-w-xs"
+              />
+            </div>
+
+            {error && (
+              <p className="text-error border border-error p-1 my-3 rounded-xl text-center">
+                {error}
+              </p>
+            )}
+
+            <button className="btn btn-wide btn-active btn-accent mx-auto my-10 text-white text-lg">
+              Login
+            </button>
+            <p>
+              New to this website?{" "}
+              <Link to="/register" className="link link-primary">
+                Register a New Account
+              </Link>
+            </p>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
