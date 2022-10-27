2ff5e7262acb6674086b19641a434d1a1eb43189
RaisulislamRimon <rimonppip0@gmail.com>
2022-10-27 20:12:51 +0600

    added environment variables in firebase config for security reason.

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDDIGB-iUc8UIYZ4eh8LVRNFmtWht4aGGs",
-  authDomain: "complete-your-code.firebaseapp.com",
-  projectId: "complete-your-code",
-  storageBucket: "complete-your-code.appspot.com",
-  messagingSenderId: "572202575188",
-  appId: "1:572202575188:web:73e3df2f1ca3a43f91d4c1",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
