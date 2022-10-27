c23f22efcfeeefc0ad358689deef40800efa98ee
RaisulislamRimon <rimonppip0@gmail.com>
2022-10-27 20:35:48 +0600

    updated faq page.

--- src/Pages/FAQ/FAQ.js
+++ src/Pages/FAQ/FAQ.js
@@ -3,7 +3,27 @@ import React from "react";
 const FAQ = () => {
   return (
     <div>
-      <h1>FAQ</h1>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">
+              Frequently Asked Questions (FAQ)
+            </h1>
+            <p className="py-6 text-justify">
+              This is the Frequently Asked Questions (FAQ) page. It is a page
+              that is used to answer the most common questions that users have
+              about your website. It is a great way to help users find the
+              information they need without having to contact you. It is also a
+              great way to help users find the information they need without
+              having to contact you. <br /> <br />
+              If you have any questions, please let us know.
+            </p>
+            <button className="btn btn-primary">
+              <a href="mailto:rimonppip0@gmail.com">Contact Us</a>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
